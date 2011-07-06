1f3af7cf1900beb88a697632fb95a1ae7af76ff6
Karl Sackett <karl.sackett@gmail.com>
2011-07-05 21:03:44 -0500

    Problem 1 completed.

--- p01.go
+++ p01.go
@@ -1,10 +1,26 @@
-/*  If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+/*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we
     get 3, 5, 6 and 9. The sum of these multiples is 23.
 
     Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-func main(args []string) {
-    code
+package main
+
+import (
+	"os"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	s := 0
+	n, _ := strconv.Atoi(os.Args[1])
+	for i := 0; i < n; i++ {
+		if (i % 3 == 0) || (i % 5 == 0) {
+			s += i
+		}
+	}
+	fmt.Println(s)
 }
 
